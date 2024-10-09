83aabc382a0f28e954ce727570fe91251419a48c
Alex Slivka <slivkaalex7@gmail.com>
2024-10-09 09:57:46 +0200

    add du

--- 04-cyklus/03-faktorial.py
+++ 04-cyklus/03-faktorial.py
@@ -3,5 +3,5 @@ faktorial = 1
 for i in range(n):
     faktorial = faktorial * (i+1) # sucet + (i+1) je výraz, najskôr sa vypočíta výraz a výsledná hodnota sa priradí do premennej sucet
     # print(i, i+1, sucet)
-print(n,"!=",faktorial)
+# print(n,"!=",faktorial)
 print(f"{n}! ={faktorial}") # zátvorky napíšeme pravý alt+b pravý alt+n {}
