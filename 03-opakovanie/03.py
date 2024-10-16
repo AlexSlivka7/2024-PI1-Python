4fff2a340fdb41e45bbdfc6337ae70708c7d1f33
Alex Slivka <slivkaalex7@gmail.com>
2024-10-16 09:19:15 +0200

    Add uloha

--- 03-opakovanie/03.py
+++ 03-opakovanie/03.py
@@ -3,3 +3,10 @@ obvod = 2 * 3.14 * r
 obsah = 3.14 * (r * r)
 print("obvod je:" , obvod)
 print("obsah je." , obsah)
+a = float(input("zadaj veľkosť strany kocky: "))
+S = ((a**2) + (a**2))
+U = S **(1/2.0)
+TU = U * a**2 
+T = TU **(1/2.0)
+print("Stenová uhlopriečka je:", round(U, 2))
+print("Telesová uhlopriečka je:",round(T, 2))
