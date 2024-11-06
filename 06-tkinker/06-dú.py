22fb298dcf222411a34da389c2ebcba5c6bd2a6e
Alex Slvika <slivkaalex7@gmail.com>
2024-11-06 17:06:29 +0100

    completed du

--- 06-tkinker/06-dú.py
+++ 06-tkinker/06-dú.py
@@ -3,41 +3,50 @@ import tkinter
 canvas = tkinter.Canvas()
 canvas.pack()
 #A:
+# horný riadok
 canvas.create_rectangle(30,10,40,20,fill="black")
 canvas.create_rectangle(42,10,52,20,fill="black")
 canvas.create_rectangle(54,10,64,20,fill="black")
-
+#ľavá noha
 canvas.create_rectangle(18,18,28,28,fill="black")
-canvas.create_rectangle(18,28,28,38,fill="black")
-canvas.create_rectangle(18,38,28,48,fill="black")
-canvas.create_rectangle(18,48,28,58,fill="black")
-canvas.create_rectangle(18,58,28,68,fill="black")
-canvas.create_rectangle(18,68,28,78,fill="black")
-
-canvas.create_rectangle(60,20,70,30,fill="black")
-canvas.create_rectangle(60,30,70,40,fill="black")
-canvas.create_rectangle(60,40,70,50,fill="black")
-canvas.create_rectangle(60,50,70,60,fill="black")
-canvas.create_rectangle(60,60,70,70,fill="black")
-canvas.create_rectangle(60,70,70,80,fill="black")
-
-canvas.create_rectangle(30,40,40,50,fill="black")
-canvas.create_rectangle(40,40,50,50,fill="black")
-canvas.create_rectangle(50,40,60,50,fill="black")
+canvas.create_rectangle(18,30,28,40,fill="black")
+canvas.create_rectangle(18,42,28,52,fill="black")
+canvas.create_rectangle(18,54,28,64,fill="black")
+canvas.create_rectangle(18,66,28,76,fill="black")
+canvas.create_rectangle(18,78,28,88,fill="black")
+#pravá noha
+canvas.create_rectangle(66,18,76,28,fill="black")
+canvas.create_rectangle(66,30,76,40,fill="black")
+canvas.create_rectangle(66,42,76,52,fill="black")
+canvas.create_rectangle(66,54,76,64,fill="black")
+canvas.create_rectangle(66,66,76,76,fill="black")
+canvas.create_rectangle(66,78,76,88,fill="black")
+#stred
+canvas.create_rectangle(30,42,40,52,fill="black")
+canvas.create_rectangle(42,42,52,52,fill="black")
+canvas.create_rectangle(54,42,64,52,fill="black")
 
 #S:
-canvas.create_rectangle(90,10,100,20,fill="black")
+#horný riadok
 canvas.create_rectangle(100,10,110,20,fill="black")
-canvas.create_rectangle(110,10,120,20,fill="black")
-canvas.create_rectangle(120,10,130,20,fill="black")
-
-canvas.create_rectangle(80,20,90,30,fill="black")
-canvas.create_rectangle(80,30,90,40,fill="black")
-
-canvas.create_rectangle(90,10,100,20,fill="black")
-canvas.create_rectangle(100,10,110,20,fill="black")
-canvas.create_rectangle(110,10,120,20,fill="black")
-
+canvas.create_rectangle(112,10,122,20,fill="black")
+canvas.create_rectangle(124,10,134,20,fill="black")
+canvas.create_rectangle(136,10,146,20,fill="black")
+#prechod ľavý
+canvas.create_rectangle(88,20,98,30,fill="black")
+canvas.create_rectangle(88,32,98,42,fill="black")
+#stredný riadok
+canvas.create_rectangle(100,44,110,54,fill="black")
+canvas.create_rectangle(112,44,122,54,fill="black")
+canvas.create_rectangle(124,44,134,54,fill="black")
+#prechod pravý
+canvas.create_rectangle(136,56,146,66,fill="black")
+canvas.create_rectangle(136,68,146,78,fill="black")
+#dolný riadok
+canvas.create_rectangle(88,80,98,90,fill="black")
+canvas.create_rectangle(100,80,110,90,fill="black")
+canvas.create_rectangle(112,80,122,90,fill="black")
+canvas.create_rectangle(124,80,134,90,fill="black")
 
 
 tkinter.mainloop()
