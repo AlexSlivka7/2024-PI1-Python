1f06619899e9bb634c6d94d7ef8da53894113dce
Alex Slvika <slivkaalex7@gmail.com>
2024-11-20 20:49:39 +0100

    modifed srdciecko

--- domov/Srdiečko.py
+++ domov/Srdiečko.py
@@ -1,12 +1,15 @@
 #Srdiečko
-
+text = input("Zadaj text: ")
+dlzka = int(input("Zadaj dĺžku: "))
+velkost = int(input("Zadaj velkost textu: "))
 import tkinter
-canvas = tkinter.Canvas(width=500, height=500)
+canvas = tkinter.Canvas(width=1000, height=1000)
 canvas.pack()
-
-canvas.create_oval(99,50,260,200,fill="red",outline="red")
-canvas.create_oval(249,50,401,200,fill="red",outline="red")
-canvas.create_polygon(100,145,400,145,250,400,fill="red")
-
+x = 20
+y = 20
+canvas.create_oval(x,y, x+dlzka, y+dlzka,fill="red",outline="red")
+canvas.create_oval(x+dlzka - x, y, x+dlzka*2 - x, y + dlzka  ,fill="red",outline="red")
+canvas.create_polygon(x + 2 , y+dlzka/2 + y   ,x + dlzka * 2 -x ,y + dlzka/2 + y,    x +dlzka - 5 ,y + dlzka * 2 ,fill="red")
+canvas.create_text(x+dlzka,y+dlzka, text=text,font=('Helvetica', velkost, 'bold'))
 
 tkinter.mainloop()
