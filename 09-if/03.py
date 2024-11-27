f2cce3f14a73ade0b12984181ee030c1a2a1c65c
Alex Slvika <slivkaalex7@gmail.com>
2024-11-27 19:38:23 +0100

    modifed 03.py

--- 09-if/03.py
+++ 09-if/03.py
@@ -9,15 +9,21 @@ for i in range(1000):
     x = random.randint(3,canvas_width-13)
     y = random.randint(3,canvas_height-13)
 
-if x > canvas_width//4 and y > canvas_height//3/4:
-    farba = "black"
-    
+    if canvas_width// 4 * 3 >= x >= canvas_width // 4 and canvas_height// 4 <= y <= canvas_height// 4 * 3:
+        farba = "black" 
+    else:
+        if x <= canvas_width // 2:
+            if y <= canvas_height // 2:
+                farba = "blue"
+            else:
+                farba = "green"
+        else:
+            if y <= canvas_height //2:
+                farba = "red"
+            else:
+                farba = "gold"
 
-
-
-
-
-canvas.create_oval(x , y, x+10, y+10, fill=farba , width=0)
+    canvas.create_oval(x , y, x+10, y+10, fill=farba , width=0)
 
 
 tkinter.mainloop()
