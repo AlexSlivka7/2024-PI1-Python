364995da5c61abb9ff8f3c8171de489d0ae2fae4
Alex Slvika <slivkaalex7@gmail.com>
2025-01-22 21:53:42 +0100

    modifed pismeno

--- 11-turtle/pismeno.py
+++ 11-turtle/pismeno.py
@@ -1,53 +1,53 @@
 import turtle
-dlzka = 50
+dlzka = int(input("Zadaj dlzku stvorca: "))
 t = turtle.Turtle()
 turtle.delay(0)
 
-def stvorec(d):
-    for i in range(4):
-     t.fd(d)
-     t.lt(90)
+def A(dlzka):
+
+    def stvorec(d):
+        for i in range(4):
+            t.fd(d)
+            t.lt(90)
 
 
-def riadok(dlzka):
-    for i in range(3):
-        stvorec(dlzka)
-        t.penup()
-        t.fd(dlzka)
-        t.pendown()
+    def riadok(dlzka):
+        for i in range(3):
+            stvorec(dlzka)
+            t.penup()
+            t.fd(dlzka)
+            t.pendown()
 
-riadok(dlzka)  
-
-t.rt(90)
-t.fd(dlzka)
-t.lt(90)
+    riadok(dlzka)  
 
 
+    def stplec(dlzka):
+        for i in range(6):
+        
+            t.rt(90)
+            t.fd(dlzka)
+            t.lt(90)
+            stvorec(dlzka)
+    stplec(dlzka)
 
+    t.penup()
+    t.fd(-4*dlzka)
+    t.lt(90)
+    t.fd(6*dlzka)
+    t.rt(90)
+    t.pendown()
 
-for i in range(6):
-   stvorec(dlzka)
-   t.rt(90)
-   t.fd(dlzka)
-   t.left(90)
-       
-       
+    stplec(dlzka)
 
-# t.penup()
-# t.fd(dlzka*-4)
-# t.pendown()
-# for i in range(6):
-#    stvorec(dlzka)
-#    t.lt(90)
-#    t.fd(dlzka)
-#    t.rt(90)
+    t.penup()
+    t.lt(90)
+    t.fd(3*dlzka)
+    t.rt(90)
+    t.fd(dlzka)
+    t.pendown()
+    riadok(dlzka)
 
-# t.fd(dlzka)
-# t.rt(90)
-# t.fd(dlzka*3)
-# t.lt(90)
-
-# riadok(dlzka)
+A(dlzka)
 
 
 turtle.mainloop()
