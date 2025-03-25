326fe674400b3c8af443df52633fc0955e287da1
Alex Slvika <slivkaalex7@gmail.com>
2025-03-25 20:03:41 +0100

    udalosti

--- 13-udalosti/12.py
+++ 13-udalosti/12.py
@@ -2,7 +2,7 @@ import tkinter
 
 def tahaj(event):
     x, y = event.x, event.y
-    canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill= farba)
+    canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill=farba.get())
 
 def zmaz(delete):
     canvas.delete("all")
@@ -13,8 +13,8 @@ def red():
 
 canvas = tkinter.Canvas()
 canvas.pack(side="left")
-farba = tkinter.Button(bg="red",command=red)
-farba.pack()
+farba = tkinter.Button(bg="red",command=red).pack()
+
 canvas.bind('<B1-Motion>', tahaj)
 canvas.bind("<ButtonPress-2>",zmaz)
 
